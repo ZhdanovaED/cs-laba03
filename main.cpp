37c6e096d443026310fa53cbd386e78eb9818c70
ZhdanovaED <62134024+ZhdanovaED@users.noreply.github.com>
2020-04-24 01:03:08 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "histogram.h"
+#include "svg.h"
+using namespace std;
+
+vector<double> input_numbers(const size_t count) {
+    vector<double> result(count);
+    for (size_t i = 0; i < count; i++) {
+        cin >> result[i];
+    }
+
+    return result;
+
+}
+
+void make_histogram(const vector<double>& numbers, double max, double min, size_t bin_count, vector <size_t>& bins )
+{
+
+    for(double x: numbers)
+    {
+        size_t bin_index=(x-min)/(max-min)*bin_count;
+        if (bin_index==bin_count)
+        {
+            bin_index--;
+        }
+        bins[bin_index]++;
+    }
+}
+
+
+vector<size_t> make_histogram(const vector<double>& numbers, const size_t count) {
+    vector<size_t> result(count);
+    double min;
+    double max;
+   find_minmax(numbers, min, max);
+    for (double number : numbers) {
+        size_t bin = (size_t)((number - min) / (max - min) * count);
+        if (bin == count) {
+            bin--;
+        }
+        result[bin]++;
+    }
+
+    return result;
+}
+
+
+
+
+
+void show_histogram_text(vector<size_t> bins) {
+    const size_t SCREEN_WIDTH = 80;
+    const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
+
+    size_t max_count = 0;
+    for (size_t count : bins) {
+        if (count > max_count) {
+            max_count = count;
+        }
+    }
+    const bool scaling_needed = max_count > MAX_ASTERISK;
+
+    for (size_t bin : bins) {
+        if (bin < 100) {
+            cout << ' ';
+        }
+        if (bin < 10) {
+            cout << ' ';
+        }
+        cout << bin << "|";
+
+        size_t height = bin;
+        if (scaling_needed) {
+            const double scaling_factor = (double)MAX_ASTERISK / max_count;
+            height = (size_t)(bin * scaling_factor);
+        }
+
+        for (size_t i = 0; i < height; i++) {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+
+}
+
+
+int main() {
+
+    size_t number_count;
+    cerr << "Enter number count: ";
+    cin >> number_count;
+
+    cerr << "Enter numbers: ";
+    const auto numbers = input_numbers(number_count);
+
+
+    size_t bin_count;
+    cerr << "Enter column count: ";
+    cin >> bin_count;
+
+
+    const auto bins = make_histogram(numbers, bin_count);
+
+   show_histogram_svg(bins);
+
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,52 @@
+#include "histogram.h"
+
+#include <cassert>
+
+void test_positive() {
+    double min = 0;
+    double max = 0;
+    find_minmax({1, 2, 3}, min, max);
+    assert(min == 1);
+    assert(max == 3);
+}
+
+void test_negative() {
+    double min = 0;
+    double max = 0;
+    find_minmax({-1, -2, -3}, min, max);
+    assert(min == -3);
+    assert(max == -1);
+}
+
+void test_threesame() {
+    double min = 0;
+    double max = 0;
+    find_minmax({1, 1, 1}, min, max);
+    assert(min == 1);
+    assert(max == 1);
+}
+
+void test_one() {
+    double min = 0;
+    double max = 0;
+    find_minmax({2}, min, max);
+    assert(min == 2);
+    assert(max == 2);
+}
+
+void test_empty() {
+    double min = 0;
+    double max = 0;
+    find_minmax({}, min, max);
+    assert(min == 0);
+    assert(max == 0);
+}
+
+int
+main() {
+    test_positive();
+    test_negative();
+    test_threesame();
+    test_one();
+    test_empty();
+}
